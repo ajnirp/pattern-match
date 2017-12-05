f3bf7ec5a924e6260ff352c27cd16cb195629011
Rohan Prinja <rohan@cs.unc.edu>
2017-12-05 01:43:36 -0500

    almost demo ready

--- app/src/main/java/com/example/rohan/patternmatch/GameActivity.java
+++ app/src/main/java/com/example/rohan/patternmatch/GameActivity.java
@@ -4,12 +4,16 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.CountDownTimer;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.GridLayout;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -41,16 +45,19 @@ public class GameActivity extends AppCompatActivity {
 
     private ArrayList<Integer> mParentButtonIDs;
 	private ArrayList<Integer> mChildButtonIDs;
+	private ArrayList<Integer> mIndividualScoreIDs;
 
     private ArrayList<Integer> mScores;
     private int mCumulativeScore;
+    private int mHighScore;
 
 	private int startPos = -1;
 	private int endPos = -1;
 	private int fromChild = -1;
 	private int toParent = -1;
 
-	private CountDownTimer mTimer;
+    private boolean mChronometerRunning;
+    private Chronometer mChronometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,6 +76,10 @@ public class GameActivity extends AppCompatActivity {
 			mDB.execSQL("INSERT into Miscellaneous VALUES('games played', '1')");
 		}
 
+        mChronometer = (Chronometer) findViewById(R.id.chrono);
+        mChronometer.start();
+        mChronometerRunning = true;
+
 		setPlayerLevelForNewGame();
 
 		int gameID = -1;
@@ -82,10 +93,14 @@ public class GameActivity extends AppCompatActivity {
 		} else {
 			mGameID = gameID;
 		}
-		
+		setHighScoreForGame();
+
 		setupChildrenAndParents();
-		
-		if (gameID != -1) {
+        mChildButtonIDs = new ArrayList<Integer>();
+        mParentButtonIDs = new ArrayList<Integer>();
+        mIndividualScoreIDs = new ArrayList<Integer>();
+
+        if (gameID != -1) {
 			loadParentsFromDB();
 		}
 		
@@ -93,10 +108,11 @@ public class GameActivity extends AppCompatActivity {
         generateButtons();
     }
 
-	private void setPlayerLevelForNewGame() {
+    private void setPlayerLevelForNewGame() {
 		Cursor c = mDB.rawQuery("SELECT * from Miscellaneous where item = 'games played'", null);
 		c.moveToFirst();
 		int gamesPlayed = Integer.valueOf(c.getString(1));
+		Log.v("Tag: ", "Game: " + String.valueOf(gamesPlayed));
 		mPlayerLevel = gamesPlayed/50 + 1;
 
 		c = mDB.rawQuery("SELECT children, columns from LevelStats where level = " + String.valueOf(mPlayerLevel), null);
@@ -117,7 +133,18 @@ public class GameActivity extends AppCompatActivity {
 		}
 		mGameID = c.getInt(0);
 	}
-	
+
+    private void setHighScoreForGame() {
+        String hsQuery = "SELECT hs FROM HighScores where gid = " + String.valueOf(mGameID);
+        Cursor c = mDB.rawQuery(hsQuery, null);
+        if(c.getCount() > 0) {
+            c.moveToFirst();
+            mHighScore = c.getInt(0);
+        } else {
+            mHighScore = mNumChildren*(mNumColumns-1);
+        }
+    }
+
 	private void setupChildrenAndParents() {
 		mChildrenValues = new ArrayList<String>();
 		mMoveHistory = new ArrayList<ArrayList<String>>();
@@ -144,7 +171,7 @@ public class GameActivity extends AppCompatActivity {
 			if (i < mNumParents) {
 				if (i < 2) {
 					for (int j = 0; j < mNumColumns; j++) {
-						if (i+j % 2 == 0) {
+						if ((i+j) % 2 == 0) {
 							defP[i] += 'A';
 						} else {
 							defP[i] += 'C';
@@ -157,16 +184,6 @@ public class GameActivity extends AppCompatActivity {
 				mParentColors.add(i);
 			}
             mChildrenColors.add(new ArrayList<Integer>());
-        }
-
-		mChildButtonIDs = new ArrayList<Integer>();
-        for(int i = 0; i < mNumChildren*mNumColumns; i++) {
-            mChildButtonIDs.add(-1);
-        }
-
-        mParentButtonIDs = new ArrayList<Integer>(mNumParents*mNumColumns);
-        for(int i = 0; i < mNumParents*mNumColumns; i++) {
-            mParentButtonIDs.add(-1);
         }
 	}
 	
@@ -214,54 +231,74 @@ public class GameActivity extends AppCompatActivity {
                 }
 				
 				for(int j = doneSoFar; j < doneSoFar+maxMatch; j++) {
-                    currChildColors.set(j, mParentColors.get(maxParent));
+                    if (currChildColors.get(j) != mParentColors.get(maxParent)) {
+                        currChildColors.set(j, mParentColors.get(maxParent));
+                        if(mChildButtonIDs.size() > i*mNumColumns+j)
+                            setButtonIcon((Button)findViewById(mChildButtonIDs.get(i*mNumColumns+j)), currChild.charAt(j), mParentColors.get(maxParent));
+                    }
 				}
                 doneSoFar += maxMatch;
             }
         }
+
+        mCumulativeScore = 0;
         for(int i = 0; i < mScores.size();i++) {
             mCumulativeScore += mScores.get(i);
+            if(mIndividualScoreIDs.size() > i)
+                ((TextView)findViewById(mIndividualScoreIDs.get(i))).setText(String.valueOf(mScores.get(i)));
+        }
+
+        if(mCumulativeScore < mHighScore) {
+            mHighScore = mCumulativeScore;
         }
         TextView highScore = (TextView) findViewById(R.id.highScore);
-        highScore.setText(String.valueOf(mCumulativeScore));
+        highScore.setText(String.valueOf(Math.min(mCumulativeScore, mHighScore)));
         TextView score = (TextView) findViewById(R.id.score);
         score.setText(String.valueOf(mCumulativeScore));
     }
 
     private void generateButtons() {
-        GridLayout gl = (GridLayout) findViewById(R.id.parents);
-        gl.setColumnCount(mNumColumns);
+        GridLayout glParent = (GridLayout) findViewById(R.id.parents);
+        glParent.setColumnCount(mNumColumns);
+        ViewGroup.MarginLayoutParams params = new ViewGroup.MarginLayoutParams(64,64);
+        params.setMargins(4,4,4,4);
 
-        GridLayout.LayoutParams lp = (GridLayout.LayoutParams) new ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         for (int i = 0; i < mNumParents; i++) {
             String parent = mParentValues.get(i);
             int color = mParentColors.get(i);
             for (int j = 0; j < mNumColumns; j++) {
                 Button btn = new Button(this);
                 int currentID = View.generateViewId();
-                mChildButtonIDs.set(i*mNumColumns+j, currentID);
+                mParentButtonIDs.add(currentID);
                 btn.setId(currentID);
+                btn.setLayoutParams(params);
                 setButtonIcon(btn, parent.charAt(j), color);
                 setParentClickListener(btn);
-                btn.setLayoutParams(lp);
-                gl.addView(btn);
+                glParent.addView(btn);
             }
         }
 
+        GridLayout glChildren = (GridLayout) findViewById(R.id.children);
+        glChildren.setColumnCount(mNumColumns+1);
         for (int i = 0; i < mNumChildren; i++) {
             String child = mChildrenValues.get(i);
             ArrayList<Integer> childColors = mChildrenColors.get(i);
             for (int j = 0; j < mNumColumns; j++) {
                 Button btn = new Button(this);
                 int currentID = View.generateViewId();
-                mChildButtonIDs.set(i*mNumColumns+j, currentID);
+                mChildButtonIDs.add(currentID);
                 btn.setId(currentID);
+                btn.setLayoutParams(params);
                 setButtonIcon(btn, child.charAt(j), childColors.get(j));
                 setChildClickListener(btn);
-                btn.setLayoutParams(lp);
-                gl.addView(btn);
+                glChildren.addView(btn);
             }
+            TextView scoreView = new TextView(this);
+            scoreView.setText(String.valueOf(mScores.get(i)));
+            int currentID = View.generateViewId();
+            mIndividualScoreIDs.add(currentID);
+            scoreView.setId(currentID);
+            glChildren.addView(scoreView);
         }
     }
 
@@ -284,11 +321,19 @@ public class GameActivity extends AppCompatActivity {
                         Toast toast = Toast.makeText(getApplicationContext(), "Invalid configuration", Toast.LENGTH_SHORT);
                         toast.show();
                     } else {
+                        ArrayList<String> oldParents = new ArrayList<String>();
+                        for(int i = 0; i < mNumParents; i++) {
+                            oldParents.add(mParentValues.get(i));
+                        }
+
+                        mMoveHistory.add(oldParents);
                         mParentValues.set(toParent, newParent);
-                        startPos = -1;
-                        endPos = -1;
-                        fromChild = -1;
+                        scoreModel();
+                        updateButtons(oldParents);
                     }
+                    startPos = -1;
+                    endPos = -1;
+                    fromChild = -1;
                 }
             }
         });
@@ -297,14 +342,22 @@ public class GameActivity extends AppCompatActivity {
     private void setChildClickListener(Button btn) {
         btn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                buttonIndex = mChildButtonIDs.indexOf(v.getId());
+                int buttonIndex = mChildButtonIDs.indexOf(v.getId());
                 if(startPos == -1 || startPos != -1 && endPos != -1) {
                     fromChild = buttonIndex/mNumColumns;
                     startPos = buttonIndex % mNumColumns;
                 } else if(endPos == -1) {
                     int newChild = buttonIndex/mNumColumns;
                     if (newChild == fromChild) {
-                        endPos = buttonIndex % mNumColumns;
+                        if((buttonIndex % mNumColumns) < startPos) {
+                            Toast toast = Toast.makeText(getApplicationContext(), "Invalid configuration", Toast.LENGTH_SHORT);
+                            toast.show();
+                            startPos = -1;
+                            endPos = -1;
+                            fromChild = -1;
+                        } else {
+                            endPos = buttonIndex % mNumColumns;
+                        }
                     } else {
                         fromChild = buttonIndex/mNumColumns;
                         startPos = buttonIndex % mNumColumns;
@@ -320,6 +373,9 @@ public class GameActivity extends AppCompatActivity {
     }
 
     private void updateHighscores() {
+        if (mHighScore > mCumulativeScore) {
+            mDB.execSQL("UPDATE HighScores SET hs = " + String.valueOf(mCumulativeScore) + " WHERE gid = " + String.valueOf(mGameID));
+        }
     }
 
     public void backToMain(View v) {
@@ -327,32 +383,48 @@ public class GameActivity extends AppCompatActivity {
 		super.onBackPressed();
 	}
 
-    private void updateUI() {
-        ViewGroup layout = (ViewGroup) findViewById(R.id.game_view);
-        for (int i = 0; i < mNumColumns; i++) {
-            Button button = new Button(this);
-//            layout.addView(button, layoutParams);
-        }
-    }
-
-	private void undoMove(View v) {
+	public void undoMove(View v) {
 		if(mMoveHistory.size() > 0) {
+            ArrayList<String> oldParents = new ArrayList<String>();
+            for(int i = 0; i < mNumParents; i++) {
+                oldParents.add(mParentValues.get(i));
+            }
 			mParentValues = mMoveHistory.get(mMoveHistory.size()-1);
 			mMoveHistory.remove(mMoveHistory.size()-1);
 			scoreModel();
+            updateButtons(oldParents);
 		} else {
             Toast toast = Toast.makeText(getApplicationContext(), "No previous moves found.", Toast.LENGTH_SHORT);
             toast.show();
 		}
 	}
 	
-	private void resetGame(View v) {
+	public void resetGame(View v) {
+        ArrayList<String> oldParents = new ArrayList<String>();
+        for(int i = 0; i < mNumParents; i++) {
+            oldParents.add(mParentValues.get(i));
+        }
 		setupChildrenAndParents();
-		
-		loadParentsFromDB();
-		
+        String[] defP = {"", ""};
+        for (int i = 0; i < mNumParents; i++) {
+            if (i < 2) {
+                for (int j = 0; j < mNumColumns; j++) {
+                    if ((i + j) % 2 == 0) {
+                        defP[i] += 'A';
+                    } else {
+                        defP[i] += 'C';
+                    }
+                }
+                mParentValues.add(defP[i]);
+            } else {
+                mParentValues.add(mChildrenValues.get(i));
+            }
+        }
 		scoreModel();
-        generateButtons();		
+        updateButtons(oldParents);
+        mChronometer.setBase(SystemClock.elapsedRealtime());
+        Toast toast = Toast.makeText(getApplicationContext(), "Game reset!!", Toast.LENGTH_SHORT);
+        toast.show();
 	}
 
     private boolean checkValidity(String newParent) {
@@ -372,15 +444,54 @@ public class GameActivity extends AppCompatActivity {
                 } else if(charCheck == 'C') {
                     cPresent = true;
                 }
+                if (aPresent && cPresent)
+                    break;
             }
-            if(!aPresent || !cPresent) {
-                return false;
+
+            if(!aPresent) {
+                for(int j = 0; j < mNumChildren; j++) {
+                    if(mChildrenValues.get(j).charAt(i) == 'A')
+                        return false;
+                }
+            }
+            if(!cPresent) {
+                for(int j = 0; j < mNumChildren; j++) {
+                    if(mChildrenValues.get(j).charAt(i) == 'C')
+                        return false;
+                }
             }
         }
         return true;
     }
 
-	private void saveGame(View v) {
+    private long convertTimeStringToSeconds(String s) {
+        String[] segments = s.split(":");
+        long multiplier = 1;
+        long result = 0;
+        for (int i = segments.length-1; i >= 0; i--) {
+            long segmentVal = Integer.valueOf(segments[i]);
+            segmentVal *= multiplier;
+            multiplier *= 60;
+            result += segmentVal;
+        }
+        return result;
+    }
+
+    public void toggle(View v) {
+        if (mChronometerRunning) {
+            mChronometer.stop();
+        } else {
+            long elapsedTime = SystemClock.elapsedRealtime();
+            String timeString = mChronometer.getText().toString().substring(6);
+            long secondsCounted = convertTimeStringToSeconds(timeString);
+            long base = elapsedTime - secondsCounted*1000;
+            mChronometer.setBase(base);
+            mChronometer.start();
+        }
+        mChronometerRunning = !mChronometerRunning;
+    }
+
+	public void saveGame(View v) {
 		String deleteQuery = "DELETE FROM SavedGames WHERE gid = " + String.valueOf(mGameID);
 		mDB.execSQL(deleteQuery);
 		
@@ -390,5 +501,21 @@ public class GameActivity extends AppCompatActivity {
 		}
 		insertQuery += ")";
 		mDB.execSQL(insertQuery);
+        Toast toast = Toast.makeText(getApplicationContext(), "Game saved!!", Toast.LENGTH_SHORT);
+        toast.show();
 	}
+
+	private void updateButtons(ArrayList<String> oldParents) {
+        for(int i = 0; i < oldParents.size(); i++) {
+            if(!oldParents.get(i).equalsIgnoreCase(mParentValues.get(i))) {
+                String oldP = oldParents.get(i);
+                String newP = mParentValues.get(i);
+                for(int j = 0; j < oldP.length(); j++) {
+                    if(oldP.charAt(j) != newP.charAt(j)) {
+                        setButtonIcon((Button)findViewById(mParentButtonIDs.get(i*mNumColumns+j)), newP.charAt(j), mParentColors.get(i));
+                    }
+                }
+            }
+        }
+    }
 }
